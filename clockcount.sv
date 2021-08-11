cf63b5023f7898661c5d538e47ac32a85e1d3255
kyusuf97 <47285417+kyusuf97@users.noreply.github.com>
2021-08-11 15:02:00 -0700

    Fixed clockcount synthesis error

--- clockcount.sv
+++ clockcount.sv
@@ -7,6 +7,7 @@ module clockcount (input logic rst, input logic scl, input logic start, input lo
 
 logic [3:0] count_next;
 logic en;
+logic [3:0] count_in;
 
 always_ff @(negedge rst, posedge start, posedge stop) begin
   if(!rst)
@@ -17,26 +18,28 @@ always_ff @(negedge rst, posedge start, posedge stop) begin
     en <= 1'b0;
 end
 
-
 always @(*) begin
   if(count >=  4'd8)
     count_next = 4'd0;
   else
-    count_next = count + 1;
+    count_next = count + 4'd1;
+end
+
+always @(*) begin
+  if(start)
+    count <= 4'd0;
+  else
+    count <= count_in;
 end
 
 always_ff @(negedge rst, posedge scl) begin
-  if(!rst || !en)
-    count <= 4'd0;
+  if(!rst)
+    count_in <= 4'd0;
   else if (en)
-    count = count_next;
+    count_in = count_next;
+  else
+    count_in = 4'd0;
 end
-
-always_ff @(negedge scl) begin
-  if(start)
-    count <= 4'd0;
-end
-
 
 
 endmodule: clockcount
